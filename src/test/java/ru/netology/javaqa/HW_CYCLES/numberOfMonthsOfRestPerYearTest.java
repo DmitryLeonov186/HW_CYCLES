130e0d71440fcb5f84dc7cc9e61f215168134f99
Dmitry Leonov <lemomonster@mail.ru>
2024-03-19 14:18:07 +0500

    Remake to parametrized test

--- src/test/java/ru/netology/javaqa/HW_CYCLES/numberOfMonthsOfRestPerYearTest.java
+++ src/test/java/ru/netology/javaqa/HW_CYCLES/numberOfMonthsOfRestPerYearTest.java
@@ -3,31 +3,26 @@ package ru.netology.javaqa.HW_CYCLES;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class numberOfMonthsOfRestPerYearTest {
 
-    @Test
-    public void shouldCalculateCondition1() {
+    @ParameterizedTest
+//    @CsvSource({
+//            "10000, 3000, 20000, 3",
+//            "100000, 60000, 150000, 2"
+//    })
+    @CsvFileSource(files="src/test/resources/numberOfMonthsOfRestPerYear.csv")
+
+    public void shouldCalculate(int income, int expenses, int threshold, int expected) {
         numberOfMonthsOfRestPerYear service = new numberOfMonthsOfRestPerYear();
 
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
-        int actual = service.calculate(income, expenses, threshold);
-
-        Assertions.assertEquals(expected, actual);
-
-    }
-
-    @Test
-    public void shouldCalculateCondition2() {
-        numberOfMonthsOfRestPerYear service = new numberOfMonthsOfRestPerYear();
-
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
+//        int income = 10_000;
+//        int expenses = 3_000;
+//        int threshold = 20_000;
+//        int expected = 3;
         int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
@@ -35,4 +30,20 @@ public class numberOfMonthsOfRestPerYearTest {
     }
 
 }
+
+//    @Test
+//    public void shouldCalculateCondition2() {
+//        numberOfMonthsOfRestPerYear service = new numberOfMonthsOfRestPerYear();
+
+//        int income = 100_000;
+//        int expenses = 60_000;
+//        int threshold = 150_000;
+//        int expected = 2;
+//        int actual = service.calculate(income, expenses, threshold);
+
+//        Assertions.assertEquals(expected, actual);
+
+//    }
+
+
 
